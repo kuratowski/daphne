9610580e3d026b91a68621c8376d1e849c177878
Joonhyung Shin <tonyshin@kaist.ac.kr>
2019-10-28 11:14:04 +0900

    Update osx_install_python.sh

--- osx_install_python.sh
+++ osx_install_python.sh
@@ -2,6 +2,7 @@
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     brew install pyenv;
-    pyenv install 3.6.8;
-    pyenv global 3.6.8;
+    pyenv install --list;
+    pyenv install 3.6.7;
+    pyenv global 3.6.7;
 fi
